39db253e353f0eba9aa3c5cfaf06d0ee72499147
Ygormorais <ygormorais@hotmail.com>
2026-06-03 16:21:39 -0300

    feat: parseJogos (HTML FPFS -> JogoFpfs[])

---
+++ lib/fpfs/types.ts
@@ -0,0 +1,27 @@
+export interface JogoFpfs {
+  fpfsJogoId: string | null
+  rodada: number
+  data: string          // ISO yyyy-mm-dd
+  hora: string | null   // "17:00"
+  ginasio: string | null
+  mandante: string
+  visitante: string
+  golsMandante: number | null
+  golsVisitante: number | null
+  sumulaUrl: string | null
+}
+
+export interface LinhaClassificacao {
+  fase: string
+  grupo: string | null
+  posicao: number
+  timeNome: string
+  pontos: number
+  jogos: number
+  vitorias: number
+  empates: number
+  derrotas: number
+  golsPro: number
+  golsContra: number
+  saldo: number
+}
